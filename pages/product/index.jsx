eb3f8061cb93920b4784665c925de4e96dd2656c
SB_ナン <nam.nguyen@sunbridge.com>
2022-03-25 13:46:48 +0900

    add prodct

---
+++ pages/product/index.jsx
@@ -0,0 +1,12 @@
+import Image from 'next/image'
+import { Grid } from '@material-ui/core'
+
+export default function Top() {
+
+    return (
+        <div>
+            product
+        </div>
+            
+    )
+  }
